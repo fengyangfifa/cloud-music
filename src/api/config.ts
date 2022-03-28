036c985363406a2537804c70439ea7fa7717df84
fengyangfifa <2314034614@qq.com>
2022-03-28 21:42:39 +0800

    chore：ts 添加 lib，引入 dom 和 es 较新的 ts 声明文件 feat：引入 axios

---
+++ src/api/config.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const baseUrl = "";
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl
+});
+
+axiosInstance.interceptors.response.use((res) => {
+  return res;
+}, (err) => {
+  console.log(err, "网络错误");
+  return Promise.reject(err);
+});
+
+export {
+  axiosInstance
+};
