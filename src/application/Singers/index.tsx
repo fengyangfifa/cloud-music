bf51337f82efe059eb484cfab838806a0c29d18b
fengyangfifa <2314034614@qq.com>
2022-04-07 23:58:27 +0800

    feat: 开发分类列表

--- src/application/Singers/index.tsx
+++ src/application/Singers/index.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Horizon from "@/baseUI/horizon-item";
+import { categoryTypes, alphaTypes } from "@/utils";
+import style from "./singers.module.scss";
 
 function Singers() {
-  return <div>Singers</div>;
+  let [category, setCategory] = useState("");
+  let [alpha, setAlpha] = useState("");
+
+  const handleUpdateAlpha = (val: string) => {
+    setAlpha(val);
+  };
+
+  const handleUpdateCategory = (val: string) => {
+    setCategory(val);
+  };
+
+  return (
+    <div className={style["nav-container"]}>
+      <Horizon
+        list={categoryTypes}
+        title="分类 (默认热门):"
+        oldVal={category}
+        handleClick={handleUpdateCategory}
+      />
+      <Horizon
+        list={alphaTypes}
+        title="首字母:"
+        oldVal={alpha}
+        handleClick={handleUpdateAlpha}
+      />
+    </div>
+  );
 }
 
 export default React.memo(Singers);
---
+++ src/baseUI/horizon-item/index.tsx
@@ -0,0 +1,45 @@
+import React, { memo, FunctionComponent } from "react";
+import clsx from "clsx";
+
+import Scroll from "@/baseUI/scroll";
+import style from "./horizon.module.scss";
+
+interface HorizonProps {
+  list: Array<{ name: string; key: string }>;
+  title: string;
+  oldVal?: string;
+  handleClick?: (val: string) => void;
+}
+
+const Horizon: FunctionComponent<HorizonProps> = (props) => {
+  const { list, oldVal, title, handleClick } = props;
+
+  return (
+    <Scroll direction={"horizental"}>
+      <div className={style.container}>
+        <div className={style.list}>
+          <span>{title}</span>
+          {list.map((item) => {
+            const selected = oldVal === item.key && style.selected;
+            return (
+              <span
+                className={clsx(style["list-item"], selected)}
+                key={item.key}
+                onClick={() => handleClick && handleClick(item.key)}
+              >
+                {item.name}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+    </Scroll>
+  );
+};
+
+Horizon.defaultProps = {
+  list: [],
+  title: ""
+};
+
+export default memo(Horizon);
