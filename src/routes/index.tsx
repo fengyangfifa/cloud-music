4a8654f523a88f83fd353d18b7679c9fb4adbb91
fengyangfifa <2314034614@qq.com>
2022-04-19 22:02:50 +0800

    feat: 明确了routes的类型

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
+import { RouteConfig } from "react-router-config";
 
 import Home from "../application/Home";
 import Recommend from "../application/Recommend";
 import Singers from "../application/Singers";
 import Rank from "../application/Rank";
 
-const routes = [
+const routes: Array<RouteConfig> = [
   {
     path: "/",
     component: Home,
