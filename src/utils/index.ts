4e9b76f9972a57e102f796b741c6dae7968c6fb4
fengyangfifa <2314034614@qq.com>
2022-04-27 22:09:28 +0800

    improvement: 整合工具函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { RankList } from "@/types";
+
 export type GetKeyType<
   T extends string,
   R extends Record<string, unknown>
@@ -203,3 +205,44 @@ export const alphaTypes = [
     name: "Z"
   }
 ];
+
+export const getCount = (count: number) => {
+  if (count < 0) return;
+
+  if (count < 10000) {
+    return count;
+  } else if (Math.floor(count / 10000) < 10000) {
+    return Math.floor(count / 1000) / 10 + "万";
+  } else {
+    return Math.floor(count / 10000000) / 10 + "亿";
+  }
+};
+
+export const debounce = (func: Function = () => {}, delay: number) => {
+  let timer: number;
+
+  return (...args: unknown[]) => {
+    if (timer) {
+      window.clearTimeout(timer);
+    }
+    timer = window.setTimeout(() => {
+      func.apply(this, args);
+      window.clearTimeout(timer);
+    }, delay);
+  };
+};
+
+export function findIndex(list: RankList) {
+  for (let i = 0; i < list.length - 1; i++) {
+    if (list[i].tracks.length && !list[i + 1].tracks.length) {
+      return i + 1;
+    }
+  }
+}
+
+export const getName = (list: Array<Record<"name", string>>) => {
+  return list.reduce((pre, cur, index) => {
+    pre += index === 0 ? cur.name : `/${cur.name}`;
+    return pre;
+  }, "");
+};
