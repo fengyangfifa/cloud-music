c337fae94db794a0a5c3201d8cd5aae38593aca6
fengyangfifa <2314034614@qq.com>
2022-04-28 13:20:49 +0800

    feat: 开发 header 组件

--- src/baseUI/header/index.tsx
+++ src/baseUI/header/index.tsx
@@ -1,28 +1,37 @@
 import React from "react";
 
+import Marquee from "@/baseUI/Marquee";
 import style from "./header.module.scss";
 
 interface HeaderProps {
   handleClick?: () => unknown;
   title?: string;
+  isMarquee?: boolean;
 }
 
 const Header = React.forwardRef<HTMLDivElement, HeaderProps>((props, ref) => {
-  const { handleClick, title } = props;
+  const { handleClick, title, isMarquee } = props;
 
   return (
     <div className={style["header-container"]} ref={ref}>
       <i className="iconfont back" onClick={handleClick}>
         &#xe655;
       </i>
-      <h1>{title}</h1>
+      {isMarquee ? (
+        <Marquee>
+          <h1>{title}</h1>
+        </Marquee>
+      ) : (
+        <h1>{title}</h1>
+      )}
     </div>
   );
 });
 
 Header.defaultProps = {
   handleClick: () => {},
-  title: "标题"
+  title: "标题",
+  isMarquee: false
 };
 
 export default React.memo(Header);
