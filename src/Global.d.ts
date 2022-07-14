5048fe9dc536f2b77908c1d50db5904629303942
fengyangfifa <2314034614@qq.com>
2022-07-14 21:51:41 +0800

    feat: 删除 Global.d.ts 中的 create-keyframe-animation 声明，安装 @types/create-keyframe-animation

--- src/Global.d.ts
+++ src/Global.d.ts
@@ -11,25 +11,3 @@ declare module "*.png" {
 interface Window {
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: Function;
 }
-
-declare module "create-keyframe-animation" {
-  interface RegisterAnimationConfig {
-    name: string;
-    animation: Record<string, Record<string, string>>;
-    presets: {
-      duration: number;
-      easing: "linear";
-    };
-  }
-
-  const animations: {
-    registerAnimation: (opts: RegisterAnimationConfig) => void;
-    runAnimation: (
-      els: HTMLElement | HTMLElement[],
-      opts: string,
-      cb?: Function
-    ) => void;
-    unregisterAnimation: (name: string) => void;
-  };
-  export default animations;
-}
